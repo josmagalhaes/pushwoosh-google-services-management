aa6f79fb2b1ab1e9ceed92b7be602b6798aac30c
Armando Gomes <armando@armandogom.es>
2020-04-02 12:45:52 +0100

    Update before_build_MoveGoogleServices.js
    logging

--- hooks/before_build_MoveGoogleServices.js
+++ hooks/before_build_MoveGoogleServices.js
@@ -25,13 +25,18 @@ if(n > 0)
     }
   }
   appId = xmlData.substring(n, n+count);
+  console.log("[PUSHWOOSH HELPER] App Identifier detected: " + appId);
 }
 
 //function to copy the file
 function copyGoogleServicesFile() {
   var srcFile = path.join("www/google-services", appId, "google-services.zip");
+  console.log("[PUSHWOOSH HELPER] Source file path: " + srcFile);
   if(fs.existsSync(srcFile)) {
+    console.log("[PUSHWOOSH HELPER] File exists.");
     fs.createReadStream(srcFile).pipe(fs.createWriteStream(path.join("www/google-services/google-services.zip")));
+  } else {
+    console.log("[PUSHWOOSH HELPER] File doesn't exist.");
   }
 };
 
